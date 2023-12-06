48b576c16bb9755ad9be63118eeca0c94adc6ce0
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-06 19:32:53 +0530

    +navbar

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel Web",
+  description: "Simple travel web",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
