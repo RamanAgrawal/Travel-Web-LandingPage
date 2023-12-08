a3bbff4e8f7f99aa056e7c0751930e5c26108c76
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-08 15:55:43 +0530

    ±Buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,17 +11,18 @@ interface ButtonI {
     | "btn_green"
     | "btn_white_text";
   icon?: string;
+  full?:boolean;
   onClick?:()=>void;
 }
-const Button = ({ title, type, icon, variant,onClick }: ButtonI) => {
+const Button = ({ title, type, icon, variant,onClick ,full}: ButtonI) => {
   return (
     <button
     onClick={onClick}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   );
 };
